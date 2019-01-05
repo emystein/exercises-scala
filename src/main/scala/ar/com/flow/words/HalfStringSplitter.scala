ecd7095c69a8f0bf3c865e7e5001a50d72f283e2
Emiliano Menéndez <emystein@gmail.com>
2019-01-05 20:39:36 -0300

    Refactor HalfStringSplitter

--- src/main/scala/ar/com/flow/words/HalfStringSplitter.scala
+++ src/main/scala/ar/com/flow/words/HalfStringSplitter.scala
@@ -3,14 +3,8 @@ package ar.com.flow.words
 object HalfStringSplitter {
   def splitWithoutCuttingWords(string: String): (String, String) = {
     val (left, right) = string.splitAt(string.length / 2)
-
-    val rightFirstSpaceIndex = right.indexOf(' ')
-
-    if (rightFirstSpaceIndex >= 0) {
-      val rightSplitAtFirstSpace = right.splitAt(rightFirstSpaceIndex)
-      (left + rightSplitAtFirstSpace._1, rightSplitAtFirstSpace._2.drop(1))
-    } else {
-      (left + right, "")
-    }
+    val rightFirstSpaceIndex = if (right.contains(' ')) right.indexOf(' ') else right.length
+    val rightSplitAtFirstSpace = right.splitAt(rightFirstSpaceIndex)
+    (left + rightSplitAtFirstSpace._1, rightSplitAtFirstSpace._2)
   }
 }
