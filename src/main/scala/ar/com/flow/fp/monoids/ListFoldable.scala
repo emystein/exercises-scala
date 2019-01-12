2d26f8812898f92c332089c554a0eda757e4e2ee
Emiliano Menéndez <emystein@gmail.com>
2019-01-12 19:25:54 -0300

    Use Foldable[Traversable] for Lists.

--- src/main/scala/ar/com/flow/fp/monoids/ListFoldable.scala
+++
@@ -1,9 +0,0 @@
-package ar.com.flow.fp.monoids
-
-class ListFoldable extends Foldable[List] {
-  override def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B = as match {
-    case Nil => z
-    case x +: Nil => f(z, x)
-    case x +: xs => f(foldLeft(xs)(z)(f), x)
-  }
-}
