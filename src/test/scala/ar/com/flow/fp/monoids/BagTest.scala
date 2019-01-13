197f35da3939989ae3cbe9e766904cb07444eebc
Emiliano Menéndez <emystein@gmail.com>
2019-01-13 18:15:56 -0300

    Implemented bag using composition of monoids.

---
+++ src/test/scala/ar/com/flow/fp/monoids/BagTest.scala
@@ -0,0 +1,11 @@
+package ar.com.flow.fp.monoids
+
+import org.scalatest.{FunSuite, Matchers}
+
+class BagTest extends FunSuite with Matchers {
+  test("BagTest") {
+    val expectedMap = Map("a" -> 2, "rose" -> 2, "is" -> 1)
+
+    Monoids.bag(Vector("a", "rose", "is", "a", "rose")) shouldBe expectedMap
+  }
+}
