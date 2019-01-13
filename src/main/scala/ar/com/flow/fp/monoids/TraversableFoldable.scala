8a601e10442c856bb9175edc771edafa064b9b6c
Emiliano Menéndez <emystein@gmail.com>
2019-01-13 11:29:11 -0300

    Cleanup TraversableFoldable

--- src/main/scala/ar/com/flow/fp/monoids/TraversableFoldable.scala
+++ src/main/scala/ar/com/flow/fp/monoids/TraversableFoldable.scala
@@ -1,11 +1,9 @@
 package ar.com.flow.fp.monoids
 
 class TraversableFoldable extends Foldable[Traversable] {
-  override def foldLeft[A, B](as: Traversable[A])(z: B)(f: (B, A) => B): B = {
-    if (as.isEmpty) {
-      z
-    } else {
-      f(foldLeft(as.tail)(z)(f), as.head)
+  override def foldLeft[A, B](as: Traversable[A])(z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil => z
+      case _ => f(foldLeft(as.tail)(z)(f), as.head)
     }
-  }
 }
