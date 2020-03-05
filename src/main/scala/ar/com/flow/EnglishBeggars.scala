b90a8d4579e8f285942990ab375f7a21ad84d57f
Emiliano Menéndez <emystein@gmail.com>
2020-03-04 22:41:59 -0300

    EnglishBeggars kata.

---
+++ src/main/scala/ar/com/flow/EnglishBeggars.scala
@@ -0,0 +1,10 @@
+package ar.com.flow
+
+import scala.util.Try
+
+// https://www.codewars.com/kata/59590976838112bfea0000fa
+object EnglishBeggars {
+   def beggars(values: List[Int], n: Int): List[Int] = {
+     Try(values.grouped(n).toList.map(_.padTo(n, 0)).transpose.map(_.sum)).getOrElse(Nil)
+  }
+}
---
+++ src/test/scala/ar/com/flow/EnglishBeggarsKataTest.scala
@@ -0,0 +1,20 @@
+package ar.com.flow
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class EnglishBeggarsKataTest extends FlatSpec with Matchers {
+  val tests = List(
+    ((List(1, 2, 3, 4, 5), 1) -> List(15)),
+    ((List(1, 2, 3, 4, 5), 2) -> List(9,6)),
+    ((List(1, 2, 3, 4, 5), 3) -> List(5,7,3)),
+    ((List(1, 2, 3, 4, 5), 6) -> List(1,2,3,4,5,0)),
+    ((List(1, 2, 3, 4, 5), 0) -> List())
+  )
+
+  tests.foreach {
+    case ((values, n), expected) =>
+      s"beggars($values, $n)" should s"return $expected" in {
+        EnglishBeggars.beggars(values, n) should be (expected)
+      }
+  }
+}
