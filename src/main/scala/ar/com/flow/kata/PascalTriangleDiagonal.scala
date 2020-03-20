e7c56917c7cc2c17651b4f84d10d495b6147b2c3
Emiliano Menéndez <emystein@gmail.com>
2020-03-20 19:49:24 -0300

    Coordinates.

--- src/main/scala/ar/com/flow/kata/PascalTriangleDiagonal.scala
+++ src/main/scala/ar/com/flow/kata/PascalTriangleDiagonal.scala
@@ -3,7 +3,7 @@ package ar.com.flow.kata
 // https://www.codewars.com/kata/559b8e46fa060b2c6a0000bf
 
 import ar.com.flow.kata.PascalTriangle.topRow
-import ar.com.flow.kata.Position.{LeftEdge, RightEdge, Top}
+import ar.com.flow.kata.PositionRelativeToEdges.{LeftEdge, RightEdge, Top}
 
 object PascalTriangle {
   val topRow: Int = 0
@@ -21,46 +21,76 @@ case class Diagonal(row: Int, diagonal: Int) {
       List.empty
     } else {
       val start = Node(row, column = diagonal + 1)
-      val ns: List[Node] = start.rightParent.map(n => Diagonal(n.row, diagonal).nodes).getOrElse(Nil)
+      val ns: List[Node] = start.rightParent.map(n => Diagonal(n.coordinates.row, diagonal).nodes).getOrElse(Nil)
       start :: ns
     }
 
   def sum: Int = nodes.map(_.value).sum
 }
 
-case class Node(row: Int, column: Int) {
-  val previousRow = row - 1
-  val previousColumn = column - 1
+object Node {
+  def apply(row: Int, column: Int): Node = {
+    Node(Coordinates(row, column))
+  }
+}
 
-  val position: Position = Position.of(this)
+case class Node(coordinates: Coordinates) {
+  def leftParent: Option[Node] = LeftParent.of(coordinates)
 
-  def leftParent: Option[Node] = LeftParent.of(this)
-
-  def rightParent: Option[Node] = RightParent.of(this)
+  def rightParent: Option[Node] = RightParent.of(coordinates)
 
   def parents: Seq[Node] = Seq(leftParent, rightParent).flatten
 
   def value: Int = parents.map(_.value).sum.max(1)
 }
 
-sealed abstract class Position extends Product with Serializable
+object LeftParent {
+  def of(coordinates: Coordinates): Option[Node] = {
+    PositionRelativeToEdges.of(CoordinatesPosition(coordinates)) match {
+      case Top() | LeftEdge() => None
+      case RightEdge()        => Some(Node(coordinates.previousRow, column = coordinates.row))
+      case _                  => Some(Node(coordinates.previousRow, coordinates.previousColumn))
+    }
+  }
+}
 
-object Position {
-  final case class Top() extends Position
-  final case class LeftEdge() extends Position
-  final case class RightEdge() extends Position
-  final case class Inside() extends Position
+object RightParent {
+  def of(coordinates: Coordinates): Option[Node] = {
+    PositionRelativeToEdges.of(CoordinatesPosition(coordinates)) match {
+      case Top() | RightEdge() => None
+      case _                   => Some(Node(coordinates.previousRow, coordinates.column))
+    }
+  }
+}
+case class Coordinates(row: Int, column: Int) {
+  val previousRow = row - 1
+  val previousColumn = column - 1
+}
 
-  def of(node: Node): Position = {
-    val isTop: Boolean = node.row == topRow
-    val isLeftEdge: Boolean = node.column == 1
-    val isRightEdge: Boolean = node.column == node.row + 1
+case class CoordinatesPosition(coordinates: Coordinates) {
+  val isTop: Boolean = coordinates.row == topRow
+  val isLeftEdge: Boolean = coordinates.column == 1
+  val isRightEdge: Boolean = coordinates.column == coordinates.row + 1
+}
 
-    if (isTop) {
+sealed abstract class PositionRelativeToEdges extends Product with Serializable
+
+object PositionRelativeToEdges {
+
+  final case class Top() extends PositionRelativeToEdges
+
+  final case class LeftEdge() extends PositionRelativeToEdges
+
+  final case class RightEdge() extends PositionRelativeToEdges
+
+  final case class Inside() extends PositionRelativeToEdges
+
+  def of(coordinatesPosition: CoordinatesPosition): PositionRelativeToEdges = {
+    if (coordinatesPosition.isTop) {
       Top()
-    } else if (isLeftEdge) {
+    } else if (coordinatesPosition.isLeftEdge) {
       LeftEdge()
-    } else if (isRightEdge) {
+    } else if (coordinatesPosition.isRightEdge) {
       RightEdge()
     } else {
       Inside()
@@ -68,21 +98,3 @@ object Position {
   }
 }
 
-object LeftParent {
-  def of(node: Node): Option[Node] = {
-    Position.of(node) match {
-      case Top() | LeftEdge() => None
-      case RightEdge()        => Some(Node(node.previousRow, column = node.row))
-      case _                  => Some(Node(node.previousRow, node.previousColumn))
-    }
-  }
-}
-
-object RightParent {
-  def of(node: Node): Option[Node] = {
-    Position.of(node) match {
-      case Top() | RightEdge() => None
-      case _                   => Some(Node(node.previousRow, node.column))
-    }
-  }
-}
